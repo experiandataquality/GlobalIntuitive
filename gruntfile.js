a021f580280ecd2a1a4e5b52c67eb0730261fc47
Andy White <andywhite87@gmail.com>
2015-10-22 12:12:13 +0100

    Add less processing to grunt watch task

--- gruntfile.js
+++ gruntfile.js
@@ -133,7 +133,7 @@ module.exports = function(grunt) {
      * Watch setup. The configured tasks will run when and of the files tested by JSHint are changed
      */
     watch: {
-      files: ['<%= jshint.files %>'],
+      files: ['<%= jshint.files %>', '<%= less.dev.options.paths %>'],
       tasks: ['jshint', 'jasmine', 'less']
     },
 
