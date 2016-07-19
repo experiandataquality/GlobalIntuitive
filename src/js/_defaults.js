ec44c8ce76e7686a658952c5b5523ffdea787052
James Bate <jbate@users.noreply.github.com>
2016-07-19 14:15:00 +0100

    Pr/21 (#23)
    * Updated validated address heading
    
    The header for when the user enters their address manually is now
    "Manual address entered". This can also be customised by the integrator.
    Addresses issue #20
    
    * Small bug fixes
    
    Removed unnecessary heading reset and fixed variable names
    
    * Showing a heading or not can now be controlled more clearly
    
    * Include showHeading setting

--- src/js/_defaults.js
+++ src/js/_defaults.js
@@ -1,7 +1,7 @@
 // Default settings
 ContactDataServices.defaults = {
 	input: { placeholderText: "Start typing an address" },
-	formattedAddress: { headingType: "h3", headingText: "Validated address" },
+	formattedAddress: { showHeading: true, headingType: "h3", validatedHeadingText: "Validated address", manualHeadingText: "Manual address entered"  },
 	editAddressText: "Edit address",
 	searchAgainText: "Search again",
 	useAddressEnteredText: "<em>Enter address manually</em>",
