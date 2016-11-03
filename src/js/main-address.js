932ee5d9a35ca529b1283e50062080a3268d4ab2
James Bate <jbate@users.noreply.github.com>
2016-11-03 15:20:51 +0000

    Hoist post-formatting-search event higher up

--- src/js/main-address.js
+++ src/js/main-address.js
@@ -406,9 +406,10 @@ ContactDataServices.address = function(customOptions){
 			// Clear search input
 			instance.input.value = "";
 
-			if(data.address.length > 0){
-				// Fire an event to say we've got the formatted address
-				instance.events.trigger("post-formatting-search", data);
+			// Fire an event to say we've got the formatted address
+			instance.events.trigger("post-formatting-search", data);
+			
+			if(data.address.length > 0){				
 
 				// Create an array to hold the hidden input fields
 				var inputArray = [];
