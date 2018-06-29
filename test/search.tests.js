d2662fc50d720104960c99aecec7ec65f720d1c2
ahmedmgamal <ahmed.gamal@ahmedgamal.info>
2018-06-29 15:42:42 +0200

    Improve Test Coverage  (#43)
    * Add elements.countryCodeMapping param: readme chanegs
    
    * Add elements.countryCodeMapping param- intial code
    
    * add build files
    
    * minor fix in readme
    
    * add test

--- test/search.tests.js
+++ test/search.tests.js
@@ -66,6 +66,47 @@ describe("ContactDataServices", function() {
 
     });
 
-  });
+    it("countryCodeMapping updates country code call to api ", function() {
 
+      var countryMap = {"GB": "GBR"};
+      var container = document.createElement("div");
+      var input = document.createElement("input");
+      container.appendChild(input);
+
+      var selectList = document.createElement("select");
+      selectList.name = "countryCode";
+      //Create and append the options   
+      var option = document.createElement("option");
+      option.value = "GB";
+      option.text = "UK";
+      selectList.appendChild(option);
+
+      option = document.createElement("option");
+      option.value = "IRL";
+      option.text = "Irland";
+      selectList.appendChild(option);
+
+      selectList.value ="GB";
+      
+      var options = {
+        token: "1234567890",
+        elements: {
+            input: input,
+            countryList: selectList,
+            countryCodeMapping:countryMap,
+          }
+        };
+     
+
+      // Act
+      var address = new ContactDataServices.address(options);
+      address.search("Term");
+      var url = ContactDataServices.urls.construct.address.search(address);
+
+      // Assert
+      expect(url.indexOf("country=GBR")>=0).toBe(true);
+
+    });
+
+  });
 });
