b9e732990811e2a2142cb4d04e98a2d2ecb697ca
John Darvill <john.darvill@experian.com>
2018-10-12 15:01:29 +0100

    Changing Datasets= to Dataset= when constructing the GI Search url.

--- dist/js/contact-data-services.js
+++ dist/js/contact-data-services.js
@@ -155,7 +155,7 @@ ContactDataServices.urls = {
         var url = ContactDataServices.urls.endpoint;
         url += "?query=" + encodeURIComponent(instance.currentSearchTerm);
         url += "&country=" + instance.currentCountryCode;
-        url += "&datasets=" + instance.currentDataSet;
+        url += "&dataset=" + instance.currentDataSet;
         url += "&take=" + (instance.maxSize || instance.picklist.maxSize);
         url += "&auth-token=" + instance.token;
         return url;
